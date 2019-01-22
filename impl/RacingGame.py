008a62e985f5a490cacca7de0622bf25da93a019
Mesut Cevik <mesut.cevik@telekom.de>
2019-01-22 15:27:47 +0100

    Exercise 11 and 12 done. Exercise 13 ongoing.

---
+++ impl/RacingGame.py
@@ -0,0 +1,8 @@
+from impl.Vehicle import Vehicle, EngineType
+
+
+class RacingGame:
+
+    if __name__ == '__main__':
+        dodgeCharger = Vehicle("Dodge", "Charger", 478, "gasoline")
+        print(dodgeCharger)
