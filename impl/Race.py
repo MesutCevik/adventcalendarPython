c79c5cfa58f28888a170e86c21b16ab20c11534b
Mesut Cevik <mesut.cevik@telekom.de>
2019-01-23 09:35:32 +0100

    Exercise 15 done, I only have to implement some code in RacingGame.

--- impl/Race.py
+++ impl/Race.py
@@ -1,18 +1,33 @@
+from random import shuffle
 from typing import List
 
 from impl import CompetitorsList, Driver, Vehicle, Competitor
 
 
 class Race:
+
     competitors: CompetitorsList
-    starting_lineup: str = list()
 
-    driver: Driver
-    vehicle: Vehicle
-
-    def race(self, competitors):
+    # CONSTRUCTOR
+    def __init__(self, competitors: CompetitorsList):
         self.competitors = competitors
-        self.starting_lineup = 0
+        self.starting_lineup: str = list()
+        self.race_result: str = list()
+
+    def race(self):
+        self.race_result = self.starting_lineup
+        shuffle(self.race_result)
+
+        places = self.race_result
+        for place in places:
+            if place == 1:
+                points = (len(self.race_result) - place + 1) * 2
+            else:
+                points = (len(self.race_result) - place +1) * 1
+
+            self.race_result[place-1].add_points(points)
+
+        self.starting_lineup = self.race_result
 
     def generate_starting_lineup(self, competitors):
         for competitor in self.competitors:  # Nehme jedes Element in einer Liste competitor.
@@ -20,14 +35,41 @@ class Race:
 
         return self.starting_lineup
 
+    def get_competitors(self):
+        return self.competitors
+
     def get_starting_lineup(self):
         return self.starting_lineup
 
+    def get_result(self):
+        return self.race_result
+
     def __str__(self):
-        nice_competitors = self.competitors
-        return f"{self.manufacturer} {self.model_name}, {self.horsepower}hsp, {self.engine_type.name}."
+        nice_competitors: str = list()
+        for competitor in self.race_result:
+            nice_competitors.__add__(f"Result Position: {[competitor.index(self.race_result)]}, {competitor}")
+            
+        return nice_competitors
 
 
+
+"""
+### Exercise 15 - Racing game race()
+  
+  Let'em race!
+   
+  1. Add and implement following methods to class `Race`:
+      * enhance the constructor with the last result so that the starting lineup can be setup correctly.
+      * _race()_ => run's the race - generate the final placement randomly
+      * _getResult()_ => race result as an ordered list of competitors. The list is ordered by the placement, like 1s is first, 2nd is second, ....
+      * _toString()_ => race result as string, like '[Result Position] [Competitor]'
+  2. Store the race result points to the `Competitor`
+      * for the first place the driver gets '[count of drivers - place + 1] * 2' points
+      * all other placements gives the drivers '[count of drivers - place + 1] * 1' points
+  3. Generate the next starting lineup from the drivers last race position.
+  4. Add useful unit tests
+  3. Visualize created classes with plantuml as class diagram, inclusive associations.
+"""
 
 
 """
